61a0502e13195d47a3c136070c0b9ef3e2e19b05
DaylinDiedericks <diedericksdaylin6@gmail.com>
2026-05-19 22:00:30 +0200

    Add files via upload

---
+++ week6_ex01_variables.js
@@ -0,0 +1,13 @@
+const firstName = "Daylin";
+const lastName = "Diedericks";
+
+let age = 20;
+
+const fullName = firstName + " " + lastName;
+
+console.log("Hello, my name is " + fullName + " and I am " + age + " years old.");
+
+age = 21;
+
+console.log("Next year, I will be " + age + "years old.");
+
