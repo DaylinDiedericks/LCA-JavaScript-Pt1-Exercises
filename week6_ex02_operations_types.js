b33b3f31f8e60eb0e77cdf59cca59616c26d3d30
DaylinDiedericks <diedericksdaylin6@gmail.com>
2026-05-20 21:36:20 +0200

    Add files via upload

---
+++ week6_ex02_operations_types.js
@@ -0,0 +1,16 @@
+let number1 = 10;
+
+let number2 = 5;
+
+let number1String = number1.toString();
+
+let sum = number1 + number2;
+
+let product = number1 * number2;
+
+console.log("number1:", number1);
+console.log("number2:", number2);
+console.log("number1 as string:", number1String);
+console.log("sum:", sum);
+console.log("product:", product);
+
