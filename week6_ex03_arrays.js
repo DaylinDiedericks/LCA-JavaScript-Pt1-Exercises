601cc3443bc1cc02e95973ffc13f560012c5a07f
DaylinDiedericks <diedericksdaylin6@gmail.com>
2026-05-21 19:59:52 +0200

    Add files via upload

---
+++ week6_ex03_arrays.js
@@ -0,0 +1,15 @@
+let colors = [];
+
+colors.push("red");
+colors.push("blue");
+colors.push("green");
+
+let numbers = [1,2,3,4,5];
+colors.pop();
+
+colors.unshift("yellow");
+
+console.log("Colors length:", colors.length);
+console.log("Numbers length:", numbers.length);
+console.log(colors);
+console.log(numbers);
